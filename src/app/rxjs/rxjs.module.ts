8c0fde1c346903379e8c5060f37ddf4a9ce9cf1d
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-08-08 18:37:13 +0400

    add rxjs module

---
+++ src/app/rxjs/rxjs.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RxjsComponent } from './rxjs.component';
+
+
+
+@NgModule({
+  declarations: [
+    RxjsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RxjsModule { }
