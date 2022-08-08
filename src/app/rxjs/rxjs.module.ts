bfb56f60e4e4a5da0bce7c434ffa950da734f81a
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-08-08 19:22:19 +0400

    update module

--- src/app/rxjs/rxjs.module.ts
+++ src/app/rxjs/rxjs.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RxjsComponent } from './rxjs.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    RxjsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [RxjsComponent],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, RouterModule],
 })
-export class RxjsModule { }
+export class RxjsModule {}
