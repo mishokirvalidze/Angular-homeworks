89b0a744416015f84d13b8c52deecf545f1b5828
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-08-10 21:00:27 +0400

    add some methods

--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from './service/http.service';
+import { AddedEmployee, Iemployee } from './model/http.model';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-http',
   templateUrl: './http.component.html',
-  styleUrls: ['./http.component.scss']
+  styleUrls: ['./http.component.scss'],
 })
 export class HttpComponent implements OnInit {
+  showForm = false;
 
-  constructor() { }
+  public employees: AddedEmployee[] = [];
+
+  employee: Iemployee = {
+    name: '',
+    age: 0,
+    salary: 0,
+  };
+
+  constructor(private ser: HttpService) {}
 
   ngOnInit(): void {
+    this.update();
   }
 
+  private update(): void {
+    this.ser
+      .getEmployees()
+      .pipe(tap((data) => (this.employees = data)))
+      .subscribe();
+  }
+
+  public delete(id: string): void {
+    this.ser.deleteEmployee(id);
+    this.update();
+  }
+
+  public post(employee: Iemployee): void {
+    this.ser.postEmployee(employee);
+    this.update();
+  }
+
+  public add(employeeData: Iemployee): void {
+    this.employee = employeeData;
+    this.post(employeeData);
+    this.cancel();
+    this.update();
+  }
+
+  public edit(id: string): void {
+    this.ser
+      .getEmployee(id)
+      .pipe(
+        tap((employeeData) => {
+          this.employee = employeeData;
+        })
+      )
+      .subscribe();
+  }
+
+  public cancel(): void {
+    this.showForm = !this.showForm;
+  }
 }
--- src/app/http/http.module.ts
+++ src/app/http/http.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpComponent } from './http.component';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { FormComponent } from './components/form/form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    HttpComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HttpComponent, EmployeesComponent, FormComponent],
+  imports: [CommonModule, HttpClientModule, RouterModule, ReactiveFormsModule],
 })
-export class HttpModule { }
+export class HttpModule {}
