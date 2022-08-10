da2944923d11ce6fb9ff496bd379fb3ab28c3752
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-08-10 20:28:21 +0400

    add form controls

--- src/app/http/components/form/form.component.ts
+++ src/app/http/components/form/form.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  NgModule,
+} from '@angular/core';
+import { Iemployee, Iform } from '../../model/http.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  @Output() cancelForm = new EventEmitter<void>();
+  @Output() addEmployee = new EventEmitter<Iemployee>();
 
-  constructor() { }
+  form = new FormGroup<Iform>({
+    name: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(2)],
+    }),
+    age: new FormControl(0, {
+      nonNullable: true,
+      validators: [Validators.required, Validators.min(16)],
+    }),
+    salary: new FormControl(0, {
+      nonNullable: true,
+      validators: [Validators.required, Validators.min(1)],
+    }),
+  });
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public cancel(): void {
+    this.cancelForm.emit();
   }
 
+  public add(): void {
+    this.addEmployee.emit(this.form.value as Iemployee);
+  }
 }
