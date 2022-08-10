a3bf56b7d9c897d8a74f790bf6efe0ae069d8876
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-08-10 21:02:03 +0400

    add id geter for edit and delete

--- src/app/http/components/employees/employees.component.ts
+++ src/app/http/components/employees/employees.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AddedEmployee } from '../../model/http.model';
 
 @Component({
   selector: 'app-employees',
@@ -6,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent implements OnInit {
+  @Input() listOfEmployees: AddedEmployee[] = [];
+
+  @Input() showForm = false;
+
+  @Output() IdForEdit = new EventEmitter<string>();
+  @Output() idForDelete = new EventEmitter<string>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  public getIdForEdit(id: string): void {
+    this.IdForEdit.emit(id);
+  }
+
+  public getIdForDelete(id: string): void {
+    this.idForDelete.emit(id);
+  }
 }
