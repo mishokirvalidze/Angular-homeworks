9716e4832ee320e2a5f2c3d10ff82594aeea9410
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-08-12 15:03:46 +0400

    add pagination

--- src/app/http/components/employees/employees.component.ts
+++ src/app/http/components/employees/employees.component.ts
@@ -14,9 +14,23 @@ export class EmployeesComponent implements OnInit {
 
   @Output() getID = new EventEmitter<string[]>();
 
+  public page = 1;
+  public count = 0;
+  public numOfEmployee = 5;
+  public employeeListSize = [5, 10, 15, 20];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  public onPageChange(event: number): void {
+    this.page = event;
+  }
+
+  public changeNum(event: Event): void {
+    this.numOfEmployee = (event.target as HTMLSelectElement)
+      .value as unknown as number;
+  }
 
   public getId(clickData: string[]): void {
     this.getID.emit(clickData);
