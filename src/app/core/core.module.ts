2c415f3d3343c372fa536d2d5bf07d39c501828c
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-08-18 11:19:02 +0400

    add page not found component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { NavigationComponent } from './component/navigation/navigation.component
 import { RouterModule } from '@angular/router';
 import { SharedService } from '../shared/shared.service';
 import { SharedModule } from '../shared/shared.module';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [NavigationComponent],
+  declarations: [NavigationComponent, PageNotFoundComponent],
   imports: [CommonModule, RouterModule, SharedModule],
   exports: [NavigationComponent],
   providers: [SharedService],
